e497f81b9f27dd79afba3cf6315cf1835f7bd43c
Martmists <martmists@gmail.com>
2018-04-05 15:12:13 +0200

    Explicit check for JRE8
    Signed-off-by: Martmists <martmists@gmail.com>

--- src/main/kotlin/com/martmists/PokemonOverlay/Main.kt
+++ src/main/kotlin/com/martmists/PokemonOverlay/Main.kt
@@ -1,8 +1,16 @@
 package com.martmists.PokemonOverlay
 
 import javafx.application.Application
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
+    try {
+        assert(System.getProperty("java.version").startsWith("1.8"))
+    } catch (e: AssertionError) {
+        println("Invalid java version, use JRE 8 instead!")
+        exitProcess(1)
+    }
+
     val screen = PokemonApp()
 
     Application.launch(screen::class.java, *args)
